52530003e5a0444c3a144349e0a74f8f6a79b8f5
anton-lapin <lapin_antoine@mail.ru>
2022-03-11 22:47:37 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Anton Lapin");
+        System.out.println("25-04-1991");
+    }
+
+}
