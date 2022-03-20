e9937f4831560550c631c3859ef8e56c77761e28
anton-lapin <lapin_antoine@mail.ru>
2022-03-20 10:31:35 +0300

    6.0.2. Заполнение массива. [#226300]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -9,6 +9,16 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println();
+        String[] names = new String[4];
+        names[0] = "Leonardo";
+        names[1] = "Raphael";
+        names[2] = "Donatello";
+        names[3] = "Michelangelo";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 
 }
