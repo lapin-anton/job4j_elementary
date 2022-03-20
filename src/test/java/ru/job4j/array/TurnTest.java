80708af144a9eb844760502a247a662ebd0f0612
anton-lapin <lapin_antoine@mail.ru>
2022-03-20 16:04:22 +0300

    6.2.3. Перевернуть массив. [#226790 #277638]

--- src/test/java/ru/job4j/array/TurnTest.java
+++ src/test/java/ru/job4j/array/TurnTest.java
@@ -13,8 +13,8 @@ public class TurnTest {
 
     @Test
     public void whenBackArrayLength10() {
-        int[] array = new int[]{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
-        int[] excepted = new int[]{5, 4, 3, 2, 1, 0, -1, -2, -3, -4, -5};
+        int[] array = new int[]{-5, -4, -3, -2, -1, 0, 1, 2, 3, 4};
+        int[] excepted = new int[]{4, 3, 2, 1, 0, -1, -2, -3, -4, -5};
         Assert.assertArrayEquals(excepted, Turn.back(array));
     }
 }
